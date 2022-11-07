210704d95d19544eb9cc1d5c0e3baef7055c66b2
neeraj <me.neeraj1455@gmail.com>
2022-11-07 12:20:47 +0530

    api-authetication

---
+++ block-BNaael/api-auth-jwt/routes/users.js
@@ -0,0 +1,45 @@
+/** @format */
+
+var express = require("express");
+var router = express.Router();
+var User = require(`../models/User`);
+
+/* GET users listing. */
+router.get("/", function (req, res, next) {
+  res.json({ message: "user information" });
+});
+
+router.post(`/register`, async (req, res, next) => {
+  try {
+    let user = await User.create(req.body);
+    res.status(200).json({ user });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post(`/login`, async (req, res, next) => {
+  var { email, password } = req.body;
+  console.log(email, password);
+
+  if (!email || !password) {
+    return res.status(400).json({ error: "email/password required" });
+  }
+  try {
+    var user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ error: "email not register" });
+    }
+    var result = await user.verifyPassword(password);
+
+    console.log(user, result);
+
+    if (!result) {
+      return res.status(400).json({ error: "invalid password" });
+    }
+    // generate token
+  } catch (error) {
+    next(error);
+  }
+});
+module.exports = router;
